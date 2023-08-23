99a97fe3403dbd6e3708847f4fe9e05655fbf391
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-23 08:05:12 -0700

    Updated BaseModel, City and State classes to work with sqlalchemy defining their table structure

--- models/city.py
+++ models/city.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Column, String, Integer,\
+        ForeignKey, relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ Defines the City class with two attributes and a relationship attribute
+        with State class.
+    """
+    __tablename__ = 'cities'
+
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
