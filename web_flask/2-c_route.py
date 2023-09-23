26df8db72b7f2d72e4aabecc064550a3e0473fd1
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-09-23 07:12:26 -0700

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+''' Starts a flask web application:
+        Listens on 0.0.0.0, port 5000
+    Routes:
+        /           - displays "Hello HBNB!"
+        /hbnb       - displays "HBNB"
+        /c/<text>   - displays "C" followed by the value of the text
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    ''' Displays Hello HBNB! '''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' Displays HBNB '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    ''' Displays "C" followed by the value of the text '''
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
